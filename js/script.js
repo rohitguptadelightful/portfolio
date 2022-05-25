f4853d3e4762594bdd3c60fe099330692b181153
rohitguptadelightfull <contact.teamrkg@gmail.com>
2022-05-25 20:27:43 +0530

    updated disclaimer updated popups

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 const header = document.querySelector("header");
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function() {
   header.classList.toggle("sticky", window.scrollY > 0);
 });
 
 let menu = document.querySelector("#menu-icon");
-let navlist= document.querySelector(".navlist");
+let navlist = document.querySelector(".navlist");
+
 
 menu.onclick = () => {
   menu.classList.toggle("bx-x");
@@ -16,13 +17,32 @@ window.onscroll = () => {
   navlist.classList.remove("active");
 }
 
-const sr = ScrollReveal ({
+const sr = ScrollReveal({
   distance: "45px",
   duration: 2700,
   reset: true
-})
+});
 
-sr.reveal(".home-text", {delay:350, origin:"left"})
-sr.reveal(".home-img", {delay:350, origin:"right"})
+sr.reveal(".home-text", {
+  delay: 350,
+  origin: "left"
+});
+sr.reveal(".home-img", {
+  delay: 350,
+  origin: "right"
+});
 
-sr.reveal(".sub-service,.about,.portfolio,.service,.cta,.contact", {delay:200, origin:"bottom"})
+sr.reveal(".sub-service,.about,.portfolio,.service,.cta,.contact", {
+  delay: 200,
+  origin: "bottom"
+});
+
+
+function openPopup(){
+  let popup = document.querySelector(".popup-section");
+  popup.classList.add("open-popup");
+}
+function closePopup(){
+  let popup = document.querySelector("#cross-icon");
+  popup.classList.remove("open-popup");
+}
